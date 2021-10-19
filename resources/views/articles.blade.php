8a28caf5904ab9aef022289a3f69c47462b2f97c
Евгений Смагин <e.smagin@iqdev.digital>
2021-10-19 15:03:45 +0500

    create templates and routes

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Статьи</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+<body>
+<div class="container mt-4">
+    <h1>Статьи</h1>
+</div>
+</body>
+</html>
